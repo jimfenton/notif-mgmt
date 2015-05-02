aca501a45ce0d88ba8855e370b4d70467a0c1dcc
Jim Fenton <fenton@bluepopcorn.net>
2015-05-01 21:45:33 -0700

    Remove category references on rules update

--- mgmt/views.py
+++ mgmt/views.py
@@ -282,9 +282,6 @@ def rules(request):
                     if r.priority == '':
                         r.priority = 0
                     r.domain = form.cleaned_data['domain']
-                    r.category = form.cleaned_data['category']
-                    if r.category == '':
-                        r.category = 0
                     r.method = form.cleaned_data['method']
                     r.save()
             return HttpResponseRedirect("rules")
